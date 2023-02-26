51b3a647a11ef5ba7ce01e0a326c8b70941bce68
Olga Yarosh <O_ll_y_a@mail.ru>
2023-02-26 20:07:38 +0300

    add def get_filtered

--- utils/func.py
+++ utils/func.py
@@ -13,3 +13,14 @@ def get_data(url):
     if response.status_code == 200:
         return response.json()
     return None, f'Ошибка при получении данных'
+
+
+def get_filtered(data):
+    """
+    Фильтрация списка, если значение state == executed
+    :param data: данные
+    :return: отфильтрованные данные по статусу операции - одобрена
+    """
+    data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
+    return data
+
